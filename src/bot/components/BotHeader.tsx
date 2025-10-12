6ab59530a2c2765ae963f0331a43a3444dd8b9ab
Grigory Nasledov <grinch131@gmail.com>
2025-10-12 17:53:34 +0300

    feat: remove min-width and header for botpanel

--- src/bot/components/BotHeader.tsx
+++ src/bot/components/BotHeader.tsx
@@ -4,7 +4,6 @@ import { ThrottlingDetector } from "./ThrottlingDetector.tsx"
 export function BotHeader() {
   return (
     <div className="header">
-      <div>AutoClick</div>
       <ThrottlingDetector />
     </div>
   )
